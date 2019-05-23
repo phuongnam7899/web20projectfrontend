5722503d5c6b6e7a138c1689a545162df45d457b
phuongnam7899 <phuongnam7899@gmail.com>
2019-05-23 17:20:06 +0700

    tách component Filter

---
+++ src/Components/Filter/FilterResult/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography'
+
+
+class FilterResult extends React.Component {
+    render(){
+        return(
+            <div>
+                <Typography variant='h4'>FIND A TUTOR</Typography>
+                <Typography variant='h4'>FIND A TUTOR</Typography>
+                <Typography variant='h4'>FIND A TUTOR</Typography>
+                <Typography variant='h4'>FIND A TUTOR</Typography>
+                <Typography variant='h4'>FIND A TUTOR</Typography>
+                <Typography variant='h4'>FIND A TUTOR</Typography>
+            </div>
+        )
+    }
+}
+
+export default FilterResult;
--- src/Components/Filter/index.js
+++ src/Components/Filter/index.js
@@ -1,29 +1,26 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import FilterBar from './FilterBar'
-import FilterColumn from './FilterColumn'
-import Typography from '@material-ui/core/Typography'
-import SessionCard from '../SessionCard'
+import FilterBar from './FilterBar';
+import FilterColumn from './FilterColumn';
+import FilterResult from './FilterResult';
 
-const Filter = () => {
-    return (
-        <div>
-            <FilterBar />
-            <Grid container xs={12} direction='row'>
-                <Grid item xs={3}>
-                    <FilterColumn />
-                </Grid>
-                <Grid xs={6} style ={{marginTop : 60}}>
-                    <Typography variant='h4'>FIND A TUTOR</Typography>
-                    <SessionCard />
-                    <SessionCard />
-                    <SessionCard />
-                    <SessionCard />
-                    <SessionCard />
-                </Grid>
 
-            </Grid>
-        </div>
-    );
+class Filter extends React.Component {
+    render(){
+        return (
+            <div>
+                <FilterBar />
+                <Grid container xs={12} direction='row'>
+                    <Grid item xs={3}>
+                        <FilterColumn />
+                    </Grid>
+                    <Grid xs={6} style ={{marginTop : 60}}>
+                        <FilterResult/>
+                    </Grid>
+    
+                </Grid>
+            </div>
+        );
+    } 
 }
 export default Filter;
