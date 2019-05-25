b5a181221a5040837e405b12ca5f66c980cd3c0e
Thau Go <thaugo32@Thaus-MacBook-Pro.local>
2019-05-25 12:31:10 +0700

    update teachingexperience

--- src/Components/Teacher/TeachingExperience.js
+++ src/Components/Teacher/TeachingExperience.js
@@ -1,63 +1,73 @@
 import React from 'react';
 import Input from '@material-ui/core/Input';
 import Grid from '@material-ui/core/Grid';
-import { Button } from '@material-ui/core';
+import { Button, FormControl } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import FormHelperText from '@material-ui/core/FormHelperText'
+import { withFormik, Form } from 'formik';
+import * as Yup from 'yup';
 
-class EditMyProfile extends React.Component {
-    state = {
-        
-    };
-
-    handleChange = name => event => {
-        this.setState({ [name]: event.target.value });
-    };
-    render() {
-        return (
-            <div>
-                <Grid container xs={12} direction="column" style={{ marginTop: 150, marginLeft : 80 }} alignItems = 'flex-start' >
-                    <Typography variant='h5'>Teaching Experience</Typography>
-                    <Grid container direction='row' xs={8} justify='flex-start' spacing={16} style={{ marginTop: 20 }} >
+const TeachingExperience = ({ values, handleChange, errors, handleBlur, touched }) => {
+    return (
+        <Form>
+            <Grid container xs={12} direction="column" style={{ marginTop: 150, marginLeft: 80 }} alignItems='flex-start' >
+                <Typography variant='h5'>Teaching Experience</Typography>
+                <Grid container direction='row' xs={8} justify='flex-start' spacing={16} style={{ marginTop: 20 }} >
                     <Grid item xs={2}>
-                        <Typography style ={{fontSize : 20}}>Year</Typography>
+                        <Typography style={{ fontSize: 20 }}>Year</Typography>
                     </Grid>
                     <Grid item xs={6}>
-                        <Typography style ={{fontSize : 20}}>Experience</Typography>
+                        <Typography style={{ fontSize: 20 }}>Experience</Typography>
                     </Grid>
                     <Grid container direction='row' xs={12} justify='flex-start' spacing={16} style={{ marginTop: 20 }} >
                         <Grid item xs={2}>
-                            <Input placeholder='Year' fullWidth />
+                            <FormControl error={!!touched.year && errors.year}>
+                                <Input placeholder='Year' name='year' value={values.year} onChange={handleChange} onBlur={handleBlur} fullWidth />
+                                <FormHelperText>{touched.year && errors.year}</FormHelperText>
+                            </FormControl>
+
                         </Grid>
                         <Grid item xs={8}>
-                            <Input placeholder='Experience' fullWidth />
-                        </Grid >
-                        <Grid item xs = {2}>
-                        <IconButton aria-label="Delete">
-                            <DeleteIcon />
-                        </IconButton>
+                            <FormControl error={!!touched.experience && errors.experience}>
+                                <Input placeholder='Experience'name='experience' value={values.experience} onChange={handleChange} onBlur={handleBlur} fullWidth />
+                                <FormHelperText>{touched.experience && errors.experience}</FormHelperText>
+                            </FormControl>
                         </Grid>
-                    </Grid>
-                    <Grid container direction='row' xs={12} justify='flex-start' spacing={16} style={{ marginTop: 10     }} >
                         <Grid item xs={2}>
-                            <Input placeholder='Year' fullWidth />
-                        </Grid>
-                        <Grid item xs={8}>
-                            <Input placeholder='Experience' fullWidth />
-                        </Grid >
-                        <Grid item xs = {2}>
-                        <IconButton aria-label="Delete">
-                            <DeleteIcon />
-                        </IconButton>
+                            <IconButton aria-label="Delete">
+                                <DeleteIcon />
+                            </IconButton>
                         </Grid>
                     </Grid>
-                    <Button style={{ backgroundColor: '#52C1C8', color: "#FFFFFF", paddingLeft : 60, paddingRight: 60}}>Add Record</Button>
+                    <FormControl>
+                        <Button type='submit' variant='extendedFab' style={{ backgroundColor: '#52C1C8', color: "#FFFFFF", paddingLeft: 60, paddingRight: 60 }}>Add Record</Button>
+                    </FormControl>
                 </Grid>
-                </Grid>
-            </div >
-        );
-    }
+            </Grid>
+        </Form>
+
+    )
 }
 
-export default EditMyProfile
+const FormikForm = withFormik({
+    mapPropsToValues({ }){
+        return{
+            year: '',
+            experience: ''
+        }
+    },
+    validationSchema: Yup.object().shape({
+        year: Yup.string()
+        .required("Year is required"),
+        experience: Yup.string()
+        .required("Experience is required")
+    }),
+    handleSubmit(values){
+        console.log(values)
+    }
+
+})(TeachingExperience)
+
+export default FormikForm
