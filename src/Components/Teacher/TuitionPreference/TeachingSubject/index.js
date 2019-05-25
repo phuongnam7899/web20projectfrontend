1d1edd4263f231a576ba60ed3332665bb8ea287e
Thau Go <thaugo32@Thaus-MacBook-Pro.local>
2019-05-25 16:05:25 +0700

    update teachingsubject

--- src/Components/Teacher/TuitionPreference/TeachingSubject/index.js
+++ src/Components/Teacher/TuitionPreference/TeachingSubject/index.js
@@ -1,122 +1,93 @@
 import React from 'react';
 import Input from '@material-ui/core/Input';
 import Grid from '@material-ui/core/Grid';
-import { Button } from '@material-ui/core';
+import { Button, FormControl } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import FormHelperText from '@material-ui/core/FormHelperText'
 import Menu from '../../../Menu'
+import { withFormik, Form } from 'formik';
+import * as Yup from 'yup';
 
-class EditMyProfile extends React.Component {
-    state = {
-
-    };
-
-    handleChange = name => event => {
-        this.setState({ [name]: event.target.value });
-    };
-    render() {
-        return (
-            <div>
-                <Grid container xs={12} direction="column" style={{ marginTop: 50, marginLeft : 80 }} alignItems = 'flex-start' >
-                    <Typography variant='h5'>Teaching Subject</Typography>
-                    <Menu name = 'Based In'/>
-                    <Grid container direction='row' xs={12} justify='flex-start' spacing={16} style={{ marginTop: 20 }} >
+const TeachingSubject = ({ values, handleChange, errors, handleBlur, touched }) => {
+    return (
+        <Form>
+            <Grid container xs={12} direction="column" style={{ marginTop: 50, marginLeft: 80 }} alignItems='flex-start' >
+                <Typography variant='h5'>Teaching Subject</Typography>
+                <Menu name='Based In' />
+                <Grid container direction='row' xs={12} justify='flex-start' spacing={16} style={{ marginTop: 20 }} >
                     <Grid item xs={2}>
-                        <Typography style ={{fontSize : 20}}>Subject</Typography>
+                        <Typography style={{ fontSize: 20 }}>Subject</Typography>
                     </Grid>
                     <Grid item xs={3}>
-                        <Typography style ={{fontSize : 20}}>Academic Level</Typography>
+                        <Typography style={{ fontSize: 20 }}>Academic Level</Typography>
                     </Grid>
                     <Grid item xs={3}>
-                        <Typography style ={{fontSize : 20}}>Grade</Typography>
+                        <Typography style={{ fontSize: 20 }}>Grade</Typography>
                     </Grid>
                     <Grid item xs={3}>
-                        <Typography style ={{fontSize : 20}}>Hourly Rate</Typography>
+                        <Typography style={{ fontSize: 20 }}>Hourly Rate</Typography>
                     </Grid>
                     <Grid container direction='row' xs={12} justify='flex-start' spacing={16} style={{ marginTop: 20 }} >
-                        <Grid item xs={2}>
-                            <Input placeholder='Subject' fullWidth />
+                        <Grid item xs={3}>
+                            <FormControl error={!!touched.subject && errors.subject}>
+                            <Input placeholder='Subject' name='subject' value={values.subject} onChange={handleChange} onBlur={handleBlur} fullWidth />
+                            <FormHelperText>{touched.subject && errors.subject}</FormHelperText>
+                            </FormControl>
                         </Grid>
                         <Grid item xs={3}>
-                            <Input placeholder='Academic Level' fullWidth />
-                        </Grid >
-                        <Grid item xs={3}>
-                            <Input placeholder='Grade' fullWidth />
-                        </Grid >
-                        <Grid item xs={2}>
-                            <Input placeholder='Hourly Rate' fullWidth />
-                        </Grid >
-                        <Grid item xs = {1}>
-                        <IconButton aria-label="Delete">
-                            <DeleteIcon />
-                        </IconButton>
-                        </Grid>
-                    </Grid>
-                    <Grid container direction='row' xs={12} justify='flex-start' spacing={16} style={{ marginTop: 20 }} >
-                        <Grid item xs={2}>
-                            <Input placeholder='Subject' fullWidth />
+                            <FormControl error={!!touched.level && errors.level}>
+                            <Input placeholder='Academic Level' name='level' value={values.level} onChange={handleChange} onBlur={handleBlur} fullWidth />
+                            <FormHelperText>{touched.level && errors.level}</FormHelperText>
+                            </FormControl>
                         </Grid>
                         <Grid item xs={3}>
-                            <Input placeholder='Academic Level' fullWidth />
-                        </Grid >
+                            <FormControl error={!!touched.grade && errors.grade}>
+                            <Input placeholder='Grade' name='grade' value={values.grade} onChange={handleChange} onBlur={handleBlur} fullWidth />
+                            <FormHelperText>{touched.grade && errors.grade}</FormHelperText>
+                            </FormControl>
+                        </Grid>
                         <Grid item xs={3}>
-                            <Input placeholder='Grade' fullWidth />
-                        </Grid >
-                        <Grid item xs={2}>
-                            <Input placeholder='Hourly Rate' fullWidth />
-                        </Grid >
-                        <Grid item xs = {1}>
-                        <IconButton aria-label="Delete">
-                            <DeleteIcon />
-                        </IconButton>
+                            <FormControl error={!!touched.rate && errors.rate}>
+                            <Input placeholder='Hourly Rate' name='rate' value={values.rate} onChange={handleChange} fullWidth />
+                            <FormHelperText>{touched.rate && errors.rate}</FormHelperText>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={1}>
+                            <IconButton aria-label="Delete">
+                                <DeleteIcon />
+                            </IconButton>
                         </Grid>
                     </Grid>
-                    <Grid container direction='row' xs={12} justify='flex-start' spacing={16} style={{ marginTop: 20 }} >
-                        <Grid item xs={2}>
-                            <Input placeholder='Subject' fullWidth />
-                        </Grid>
-                        <Grid item xs={3}>
-                            <Input placeholder='Academic Level' fullWidth />
-                        </Grid >
-                        <Grid item xs={3}>
-                            <Input placeholder='Grade' fullWidth />
-                        </Grid >
-                        <Grid item xs={2}>
-                            <Input placeholder='Hourly Rate' fullWidth />
-                        </Grid >
-                        <Grid item xs = {1}>
-                        <IconButton aria-label="Delete">
-                            <DeleteIcon />
-                        </IconButton>
-                        </Grid>
-                    </Grid>
-                    <Grid container direction='row' xs={12} justify='flex-start' spacing={16} style={{ marginTop: 20 }} >
-                        <Grid item xs={2}>
-                            <Input placeholder='Subject' fullWidth />
-                        </Grid>
-                        <Grid item xs={3}>
-                            <Input placeholder='Academic Level' fullWidth />
-                        </Grid >
-                        <Grid item xs={3}>
-                            <Input placeholder='Grade' fullWidth />
-                        </Grid >
-                        <Grid item xs={2}>
-                            <Input placeholder='Hourly Rate' fullWidth />
-                        </Grid >
-                        <Grid item xs = {1}>
-                        <IconButton aria-label="Delete">
-                            <DeleteIcon />
-                        </IconButton>
-                        </Grid>
-                    </Grid>
-                    
-                    <Button style={{ backgroundColor: '#52C1C8', color: "#FFFFFF", paddingLeft : 60, paddingRight: 60}}>Add Record</Button>
+                    <Button type="submit" style={{ backgroundColor: '#52C1C8', color: "#FFFFFF", paddingLeft: 60, paddingRight: 60 }}>Add Record</Button>
                 </Grid>
-                </Grid>
-            </div >
-        );
-    }
+            </Grid>
+        </Form>
+    )
 }
 
-export default EditMyProfile
+const FormikDefault = withFormik({
+    mapPropsToValues({ }) {
+        return ({
+            subject: '',
+            level: '',
+            rate: '',
+            grade: ''
+        })
+    },
+    validationSchema: Yup.object().shape({
+        subject: Yup.string()
+        .required('Subject is required'),
+        level: Yup.string()
+        .required('Level ís required'),
+        grade: Yup.string()
+        .required('Grade is required'),
+        rate: Yup.string(),
+    }),
+    handleSubmit(values){
+        console.log(values)
+    }
+})(TeachingSubject)
+
+export default FormikDefault
